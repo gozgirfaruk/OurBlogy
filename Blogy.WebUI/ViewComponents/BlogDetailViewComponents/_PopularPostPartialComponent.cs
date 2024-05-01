3da7102476f31feaf5d03bf65549c02780f1e9a7
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2024-05-01 14:18:14 +0300

    bazı eksikler tamamlandı

---
+++ Blogy.WebUI/ViewComponents/BlogDetailViewComponents/_PopularPostPartialComponent.cs
@@ -0,0 +1,27 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Blogy.DataAccessLayer.Context;
+using Blogy.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.ViewComponents.BlogDetailViewComponents
+{
+    public class _PopularPostPartialComponent : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+
+        public _PopularPostPartialComponent(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = _articleService.TGetyById(id).AppUserID;
+            using var c = new BlogyContext();
+            var popular = c.Articles.Where(x=>x.AppUserID==values).ToList();
+            ViewBag.i = id;
+            return View(popular);
+        }
+    }
+}
